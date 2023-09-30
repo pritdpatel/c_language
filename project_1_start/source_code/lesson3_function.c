c81c3b8212bc05eb49ae7b10886e4d715176d363
pritdpatel <ppd2852@gmail.com>
2023-09-30 13:59:04 -0400

    date:30 september 2023

--- project_1_start/source_code/lesson3_function.c
+++ project_1_start/source_code/lesson3_function.c
@@ -2,10 +2,15 @@
 void task1();
 void task2(int);
 int task3(int, int);
+void task4(int* );
 int main(){
     task1();// print welcome
     task2(10);  //pass data
-    task2(task3(4,12));
+    task2(task3(4,12)); 
+    int Int=21;
+    int* Pointer;
+    Pointer=&Int;
+    task4(Pointer); //reference 
     return 0;
 }
 
@@ -16,6 +21,9 @@ void task1(){
 void task2(int data){
     printf("\t\t%d\n",data);
 }
- int task3(int a, int b){
+int task3(int a, int b){
     return a+b;
+}
+ void task4(int* p){
+    printf("\t\t%d \n", *p);
  }
