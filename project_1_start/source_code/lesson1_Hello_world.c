d51cf9cb029eadac9339a3bc8a21fec5b41a022b
pritdpatel <ppd2852@gmail.com>
2023-09-30 11:35:11 -0400

    date:30 september 2023

---
+++ project_1_start/source_code/lesson1_Hello_world.c
@@ -0,0 +1,17 @@
+/* 
+*   this line add stdio.h file into this working file before compilation
+*/
+#include<stdio.h>
+/*
+*   main is the function which is run by default. it will return integer vaule. and there is 0 argument.
+*/
+int main(){
+    /*
+    *   printf function is fetched from stdio.h file which have its algorithem. there is  argument "Hello world " and datatype of it is String or char[]
+    */
+    printf("Hello world ");
+    /*
+    *   this function will return 0 if every thing error free.
+    */
+    return 0;
+}
